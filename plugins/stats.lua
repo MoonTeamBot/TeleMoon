a81499cc0a4919b121bfd8d78b2d5b4ef095b84b
hjsrzvpqma <makan.j100100@gmail.com>
2016-03-20 02:30:32 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'cyclone' then -- Put everything you like :)
+  if matches[1]:lower() == 'gem' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "cyclone" then -- Put everything you like :)
+    if matches[2] == "gem" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -144,7 +144,12 @@ return {
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
     "^[#!/]([Ss]tats) (cyclone)",
-	"^[#!/]([Cc]yclone)"
+	"^[#!/]([Cc]yclone)",
+    "^[#!/]([Ss]tats)$",
+    "^[#!/]([Ss]tatslist)$",
+    "^[#!/]([Ss]tats) (group) (%d+)",
+    "^[#!/]([Ss]tats) (gem)",
+	"^[#!/]([Gg]em)"
     }, 
   run = run
 }
