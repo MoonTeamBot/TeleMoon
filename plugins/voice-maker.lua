b1ff1f81f4a787c56c55086d9073a84509792261
hjsrzvpqma <makan.j100100@gmail.com>
2016-04-18 01:11:16 +0430

    Update voice-maker.lua

--- plugins/voice-maker.lua
+++ plugins/voice-maker.lua
@@ -1,18 +1,37 @@
 do
-local function run(msg, matches)
-  local url = "http://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text="..matches[1]
-  local receiver = get_receiver(msg)
-  local file = download_to_file(url,'text.ogg')
-      send_audio('channel#id'..msg.to.id, file, ok_cb , false)
-end
 
+local function run(msg, matches)
+
+
+local text = matches[1]
+
+  local b = 1
+
+  while b ~= 0 do
+    textc = text:trim()
+    text,b = text:gsub(' ','.')
+    
+    
+  if msg.to.type == 'user' then 
+      return nil
+      else
+  local url = "http://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text="..textc
+  local receiver = get_receiver(msg)
+  local file = download_to_file(url,'Hextor.ogg')
+      send_audio('chat#id'..msg.to.id, file, ok_cb , false)
+end
+end
+  end
 return {
   description = "text to voice",
   usage = {
     "!voice [text]"
   },
   patterns = {
-    "^[!/#]voice (.+)$"
+    "^!voice +(.*)$",
+    "^/voice +(.*)$",
+	"^#voice +(.*)$",
+
   },
   run = run
 }
