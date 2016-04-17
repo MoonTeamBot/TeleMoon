af274488d444bdfef48e394a1cb37cac75516780
hjsrzvpqma <makan.j100100@gmail.com>
2016-04-18 01:01:09 +0430

    Create getwlc.lua

---
+++ plugins/getwlc.lua
@@ -0,0 +1,22 @@
+local function run(msg, matches, callback, extra)
+
+local data = load_data(_config.moderation.data)
+
+local group_welcome = data[tostring(msg.to.id)]['group_welcome']
+
+if matches[1] == 'chat_add_user' or 'chat_add_user_link' then --For Normal Group
+    if not msg.service then 
+        return nil 
+    else
+        return group_welcome
+end
+end
+end
+return {
+  patterns = {
+  "^!!tgservice (chat_add_user)$",
+  "^!!tgservice (chat_add_user_link)$",
+  },
+  run = run,
+  pre_process = pre_process
+}
