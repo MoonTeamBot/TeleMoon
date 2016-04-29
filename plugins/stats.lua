6d1680b12b1fab48985e7caa6bdb51e921051ee7
hjsrzvpqma <makan.j100100@gmail.com>
2016-04-29 04:31:03 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'gem' then -- Put everything you like :)
+  if matches[1]:lower() == 'moon' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "gem" then -- Put everything you like :)
+    if matches[2] == "moon" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,13 +143,13 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (cyclone)",
-	"^[#!/]([Cc]yclone)",
-    "^[#!/]([Ss]tats)$",
-    "^[#!/]([Ss]tatslist)$",
-    "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (gem)",
-	"^[#!/]([Gg]em)"
+    "^[#!/]([Ss]tats) (moon)",
+	"^[#!/]([Mm]oon)",
+    "^([Ss]tats)$",
+    "^([Ss]tatslist)$",
+    "^([Ss]tats) (group) (%d+)",
+    "^([Ss]tats) (moon)",
+	"^([Mm]oon)"
     }, 
   run = run
 }
