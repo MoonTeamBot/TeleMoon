4ddbae2532600942047ea71a680c075bbdc9f3a8
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-04 00:48:52 +0430

    Create lock_photo.lua

---
+++ plugins/lock_photo.lua
@@ -0,0 +1,31 @@
+
+--An empty table for solving multiple kicking problem(thanks to @topkecleon )
+kicktable = {}
+
+
+local function run(msg, matches)
+    if is_momod(msg) then
+        return msg
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_photo'] then
+                lock_ax = data[tostring(msg.to.id)]['settings']['lock_photo']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_ax == "yes" then
+        send_large_msg(get_receiver(msg), "User @" .. msg.from.username .. " images share is not allowed here!")
+        chat_del_user(chat, user, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+  "%[(photo)%]"
+ },
+  run = run
+}
