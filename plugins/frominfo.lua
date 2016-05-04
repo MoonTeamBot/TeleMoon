0d6a10f5882d103a773e42fb89171cb2752aee97
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-04 04:40:08 +0430

    Update frominfo.lua

--- plugins/frominfo.lua
+++ plugins/frominfo.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return   "نام کامل:  "..(msg.from.print_name or -----).."\nنام کوچک: "..(msg.from.first_name or -----).."\nنام خانوادگی:  "..(msg.from.last_name or -----).."\n\nشماره موبایل: +"..(msg.from.phone or -----).."\nکشور: جمهوری اسلامی ایران\nنوع سیمکارت: "..(msg.from.sim or -----).."\nیوزرنیم: @"..(msg.from.username or -----).."\nآی دی: "..msg.from.id.."\nمقام: -----\nجایگاه: -----\nرابط کاربری: موبایل\nنام گروه: "..msg.to.title.."\nآی دی گروه: 
+  return   "نام کامل:  "..(msg.from.print_name or _____).."\nنام کوچک: "..(msg.from.first_name or -----).."\nنام خانوادگی:  "..(msg.from.last_name or -----).."\n\nشماره موبایل: +"..(msg.from.phone or -----).."\nکشور: جمهوری اسلامی ایران\nنوع سیمکارت: "..(msg.from.sim or -----).."\nیوزرنیم: @"..(msg.from.username or -----).."\nآی دی: "..msg.from.id.."\nمقام: -----\nجایگاه: -----\nرابط کاربری: موبایل\nنام گروه: "..msg.to.title.."\nآی دی گروه: 
 "..msg.to.id..
 end
 return {
@@ -9,7 +9,7 @@ return {
   usage = "",
   patterns = {
     "^[Ii]nfo$",
-    "^![Ii]nfo$",
+    "^[/!#][Ii]nfo$",
   },
   run = run
 }
