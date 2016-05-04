f80290a2661e2ae84e03c276b6116dc95622d7ab
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-04 05:04:01 +0430

    Update frominfo.lua

--- plugins/frominfo.lua
+++ plugins/frominfo.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
   return   "نام کامل:  "..(msg.from.print_name or _____).."\nنام کوچک: "..(msg.from.first_name or _____).."\nنام خانوادگی:  "..(msg.from.last_name or _____).."\n\nشماره موبایل: +"..(msg.from.phone or _____).."\nکشور: جمهوری اسلامی ایران\nنوع سیمکارت: "..(msg.from.sim or _____).."\nیوزرنیم: @"..(msg.from.username or _____).."\nآی دی: "..msg.from.id.."\n\nمقام: _____\nجایگاه: _____\n\nرابط کاربری: موبایل\nتعداد پیامها: "..user_info_msgs.."\n\nنام گروه: "..msg.to.title.."\nآی دی گروه: 
-"..msg.to.id..
+"..msg.to.id.."
 end
 return {
   description = "", 
