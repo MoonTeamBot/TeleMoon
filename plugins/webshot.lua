def674846b6a3557c1d36b1794a55884438ae2be
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-05 22:17:49 +0430

    Create webshot.lua

---
+++ plugins/webshot.lua
@@ -0,0 +1,19 @@
+do
+
+local function run(msg, matches)
+  local eq = URL.escape(matches[1])
+
+  local url = "http://api.screenshotmachine.com/?key=b645b8&size=NMOB&url="..eq
+
+  local receiver = get_receiver(msg)
+  send_photo_from_url(receiver, url, send_title, {receiver, title})
+end
+
+return {
+  patterns = {
+    "^[Ww]eb (.+)$"
+  },
+  run = run
+}
+
+end
