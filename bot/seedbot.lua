88d98c120f9f674114c68041abd4b492cb858be5
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-05 21:56:35 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -233,7 +233,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {90285047,184061737},--Sudo users
+    sudo_users = {90285047},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[TeleMoon V4
 An advanced administration bot based on TG-CLI written in Lua
