d1f268cb985de2e27890445ad96e9a80cac932aa
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-13 11:03:16 +0430

    Update Html.lua

--- plugins/Html.lua
+++ plugins/Html.lua
@@ -1,13 +1,13 @@
-local function run(msg, matches)
- if matches[1]:lower() == 'code' then
-  local url = http.request('http://www.iwebtool.com/code_viewer'..URL.escape(matches[2]))
-  return text
- end
+local function run(msg, maches)
+  if maches[1] == "members" then
+   return file
 end
 
-return {
-   patterns = {
-"^[/!](code) (.*)$",
-   },
-   run = run
+<html><head><meta charset="UTF-8"><title>Umbrella Copy</title></head><body bgcolor="#404040"><center><font size=5 face=tahoma color=#cc0030><b>Umbrella Copy - Members Log Pro</b></font><br><font size=3 face=tahoma color=#ffffff>Group Name: <b>add,plug</b><br>Group ID: <b>134061639</b><br>People: <b>5</b></font><br><br><font size=2 face=tahoma color=#000000><table width=750 bgcolor="#efefef" border=1 cellSpacing=1 cellPadding=1><tr><td width="5%" align="center" valign="middle" bgcolor="#cc0030"><font color=#ffffff><b>#</b></font></td><td width="40%" align="center" valign="middle" bgcolor="#cc0030"><font color=#ffffff><b>Last and First Name</b></font></td><td width="22%" align="center" valign="middle" bgcolor="#cc0030"><font color=#ffffff><b>Username</b></font></td><td width="18%" align="center" valign="middle" bgcolor="#cc0030"><font color=#ffffff><b>Phone Num</b></font></td><td width="15%" align="center" valign="middle" bgcolor="#cc0030"><font color=#ffffff><b>ID</b></font></td></tr><tr><td
+end
+
+  patterns = {
+  "^members$",
+  },
+  run = run
 }
