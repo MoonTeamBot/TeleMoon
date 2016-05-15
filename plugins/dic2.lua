6e27d87f41cd85fcf158c27f13c651865ffd50f0
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-15 16:47:34 +0430

    Update dic2.lua

--- plugins/dic2.lua
+++ plugins/dic2.lua
@@ -1,7 +1,7 @@
 do
 local function run(msg, matches)
 if #matches == 1 then
-local path = https.request('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160119T111342Z.fd6bf13b3590838f.6ce9d8cca4672f0ed24f649c1b502789c9f4687a&format=plain&lang=fa&text='..URL.escape(matches[2]))
+local htp = https.request('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160119T111342Z.fd6bf13b3590838f.6ce9d8cca4672f0ed24f649c1b502789c9f4687a&format=plain&lang=fa&text='..URL.escape(matches[2]))
 local data = json:decode(htp)
 return ''..data.text[1]
 elseif #matches == 2 then
