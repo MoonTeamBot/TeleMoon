5d2fc687e0c0f1ea5a9d9b2edd836f23c81319c6
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-15 16:46:07 +0430

    Update dic2.lua

--- plugins/dic2.lua
+++ plugins/dic2.lua
@@ -1,11 +1,11 @@
 do
 local function run(msg, matches)
 if #matches == 1 then
-local https = https.request('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160119T111342Z.fd6bf13b3590838f.6ce9d8cca4672f0ed24f649c1b502789c9f4687a&format=plain&lang=fa&text='..URL.escape(matches[2]))
+local path = https.request('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160119T111342Z.fd6bf13b3590838f.6ce9d8cca4672f0ed24f649c1b502789c9f4687a&format=plain&lang=fa&text='..URL.escape(matches[2]))
 local data = json:decode(htp)
 return ''..data.text[1]
 elseif #matches == 2 then
-local https = https.request('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160119T111342Z.fd6bf13b3590838f.6ce9d8cca4672f0ed24f649c1b502789c9f4687a&format=plain&lang='..URL.escape(matches[1])..'&text='..URL.escape(matches[2]))
+local htp = https.request('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160119T111342Z.fd6bf13b3590838f.6ce9d8cca4672f0ed24f649c1b502789c9f4687a&format=plain&lang='..URL.escape(matches[1])..'&text='..URL.escape(matches[2]))
 local data = json:decode(htp)
 return ''..data.text[1]
 end
