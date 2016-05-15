254aedf3b3aad8c21a9781f4752d27813b547d37
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-15 17:39:45 +0430

    Update send.lua

--- plugins/send.lua
+++ plugins/send.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
 	if matches[1] == 'send' and is_sudo(msg) then
-		local response = matches[3]
-		send_large_msg("channel#id"..matches[2], response)
+		local channel = matches[3]
+		send_large_msg("channel#id"..matches[2], channel)
 	end
 	end
 return {
