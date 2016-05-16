eba816b864cc9c927d103a8f6724a66ce0bc3db4
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-16 17:27:02 +0430

    Update tophoto.lua

--- plugins/tophoto.lua
+++ plugins/tophoto.lua
@@ -1,4 +1,4 @@
-local function toimage(msg, success, result)
+local function tophoto(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
     local file = '/root/blackplus/data/stickers/'..msg.from.id..'.jpg'
@@ -20,7 +20,7 @@ local function run(msg,matches)
       		if redis:set("sticker:photo", "waiting") then
       		end
   	end
-      if matches[1] == "toimage" then
+      if matches[1] == "tophoto" then
     	redis:get("sticker:photo")  
         load_document(msg.reply_id, toimage, msg)
     end
