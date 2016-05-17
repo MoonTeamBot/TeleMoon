c89904f7d3fe6b6f6d5bcf430d9d05f82f135fa8
Moon Project <makan.j120120@gmail.com>
2016-05-17 22:56:42 +0430

    Update moon.lua

--- plugins/moon.lua
+++ plugins/moon.lua
@@ -21,7 +21,7 @@ local space = '______________________________'
 if not group_link then
 return ''
 end
-return "مشحصات فنی سرور\n مدت آنلاین :"..uptime.."\nRedstation\nCPU : 2 Core\nRAM : 2 GB\nHDD : 2 x 4 TB\nIPN : 9 MB/S\nPort : 4 MB/s\n"..space.."\nنرخ گروه با ربات ضد اسپم moon:\n گروه وی آی پی = 2000 تومان\n"..space.."\n توسئه دهندگان :\nسازنده و صاحب امتیاز: #MAKAN\n"..admins.."\n"..space.."\nپلهای ارتباطی :\nکانال : https://telegram.me/MoonTeam\n لینک ساپورت :\n"..group_link
+return "مشحصات فنی سرور\n مدت آنلاین :"..uptime.."\nRedstation\nCPU : 2 Core\nRAM : 2 GB\nHDD : 2 x 4 TB\nIPN : 9 MB/S\nPort : 4 MB/s\n"..space.."\nنرخ گروه با ربات ضد اسپم moon:\n گروه وی آی پی = 5000 تومان\n"..space.."\n توسئه دهندگان :\nسازنده و صاحب امتیاز: #MAKAN\n"..admins.."\n"..space.."\nپلهای ارتباطی :\nکانال : https://telegram.me/MoonTeam\n لینک ساپورت :\n"..group_link
 end
 return {
 patterns = {
