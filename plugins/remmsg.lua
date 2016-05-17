ae0c11f14a8804b2635837aed4a457e05bc0b821
TelegramDev <makan.j110110@gmail.com>
2016-05-17 21:53:40 +0430

    Update remmsg.lua

--- plugins/remmsg.lua
+++ plugins/remmsg.lua
@@ -3,23 +3,23 @@ for i=1, #result do
 delete_msg(result[i].id, ok_cb, false)
 end
 if tonumber(extra.con) == #result then
-send_msg(extra.chatid, ''..#result..' پیام اخیر سوپر گروه حذف شد', ok_cb, false)
+send_msg(extra.chatid, ''..#result..' msg removed', ok_cb, false)
 else
-send_msg(extra.chatid, 'تمام پیام های سوپر گروه حذف شد', ok_cb, false)
+send_msg(extra.chatid, 'all supergroup msgs removed', ok_cb, false)
 end
 end
 local function run(msg, matches)
 if matches[1] == 'rmmsg' and is_owner(msg) then
             if msg.to.type == 'channel' then
             if tonumber(matches[2]) > 9999 or tonumber(matches[2]) < 1 then
-            return "خطا عدد انتخاب شده باید زیر 100 باشد"
+            return "error!"
             end
             get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
         else
-                         return "فقط مخصوص سوپرگروه می باشد"
+                         return "only for supergroups"
         end
 else
-return "فقط مخصوص مدیران گروه می باشد"
+return "only for mods"
 end
 end
 return {
