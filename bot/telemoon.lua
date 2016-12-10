9ca5d3df2fd85af42227380a7a7764c2cd4250c1
ABOL1378 <ABOLFAZL.ANVARI24@gmail.com>
2016-12-10 13:10:39 -0800

    Update telemoon.lua

--- bot/telemoon.lua
+++ bot/telemoon.lua
@@ -233,7 +233,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {90285047,211606126,65609166},--Sudo users
+    sudo_users = {275579349},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[TeleMoon V.4
 An advanced administration bot based on TG-CLI written in Lua Open source!
